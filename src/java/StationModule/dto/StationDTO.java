1ac66fb0ac748bc7fe51c975395b7baa2e975bbc
HUYNQHE194816 <ngoquanghuy925@gmail.com>
2025-10-26 14:10:55 +0700

    Create Station DTO

---
+++ src/java/StationModule/dto/StationDTO.java
@@ -0,0 +1,61 @@
+package StationModule.dto;
+
+import java.util.List;
+
+public class StationDTO {
+
+    private int stationId;
+    private String stationName;
+    private String location;
+    private String openTime;
+    private String closeTime;
+    private List<String> routeNames;
+
+    public int getStationId() {
+        return stationId;
+    }
+
+    public void setStationId(int stationId) {
+        this.stationId = stationId;
+    }
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public void setStationName(String stationName) {
+        this.stationName = stationName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getOpenTime() {
+        return openTime;
+    }
+
+    public void setOpenTime(String openTime) {
+        this.openTime = openTime;
+    }
+
+    public String getCloseTime() {
+        return closeTime;
+    }
+
+    public void setCloseTime(String closeTime) {
+        this.closeTime = closeTime;
+    }
+
+    public List<String> getRouteNames() {
+        return routeNames;
+    }
+
+    public void setRouteNames(List<String> routeNames) {
+        this.routeNames = routeNames;
+    }
+}
