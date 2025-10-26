c2078220f9f1f372dbf32e6bf08158af3a665c18
HUYNQHE194816 <ngoquanghuy925@gmail.com>
2025-10-26 14:11:20 +0700

    Create Station Services

---
+++ src/java/StationModule/services/StationServices.java
@@ -0,0 +1,44 @@
+package StationModule.services;
+
+import StationModule.dao.StationDAO;
+import StationModule.model.Station;
+import java.util.List;
+
+public class StationServices {
+
+    private final StationDAO stationDAO;
+
+    public StationServices() {
+        this.stationDAO = new StationDAO();
+    }
+
+    // Lấy toàn bộ danh sách trạm
+    public List<Station> getAllStations() {
+        return stationDAO.getAll();
+    }
+
+    // Lấy thông tin trạm theo ID
+    public Station getStationById(int id) {
+        return stationDAO.getById(id);
+    }
+
+    // Tìm trạm theo tên
+    public List<Station> findStationsByName(String name) {
+        return stationDAO.getByName(name);
+    }
+
+    // Thêm trạm mới
+    public boolean createStation(Station station) {
+        return stationDAO.create(station);
+    }
+
+    // Cập nhật trạm
+    public boolean updateStation(Station station) {
+        return stationDAO.update(station);
+    }
+
+    // Xóa trạm
+    public boolean deleteStation(int id) {
+        return stationDAO.delete(id);
+    }
+}
