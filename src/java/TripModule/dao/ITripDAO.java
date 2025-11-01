a85208c6eecfc6f620a40bcd49d9bd77f5bdac3e
ttrangdt <ttrang.wk@gmail.com>
2025-11-01 13:53:48 +0700

    ITripDAO

---
+++ src/java/TripModule/dao/ITripDAO.java
@@ -0,0 +1,62 @@
+
+package TripModule.dao;
+
+import TripModule.model.Trip;
+import java.sql.*;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface ITripDAO {
+
+    //CRUD
+    public boolean insertTrip(Trip trip) throws SQLException;
+
+    public boolean updateTrip(Trip trip) throws SQLException;
+
+    public boolean deleteTrip(int tripId) throws SQLException;
+
+    public Trip findTripById(int tripID) throws SQLException;
+
+    //List & Filter
+    public List<Trip> findAllTrips() throws SQLException;
+
+    public List<Trip> findTrips(String search, String filter, String sort, int page, int pageSize) throws SQLException;
+
+    public int countTrips(String search, String filter) throws SQLException;
+
+    //Detail view
+    public Trip getTripDetail(int tripId) throws SQLException;
+
+    //Assignment
+    public boolean assignRoute(int tripId, int routeId) throws SQLException;
+
+    public boolean assignBus(int tripId, int busId) throws SQLException;
+
+    public boolean assignDriver(int tripId, UUID driverId) throws SQLException;
+
+    public boolean assignConductor(int tripId, UUID conductorId) throws SQLException;
+
+    //Status & Time
+    public boolean updateTripTime(int tripId, LocalTime departureTime, LocalTime arrivalTime) throws SQLException;
+
+    public boolean updateTripStatus(int tripId, String status) throws SQLException;
+
+    //Find
+    List<Trip> findTripsByDriver(UUID driverId) throws SQLException;
+
+    List<Trip> findTripsByConductor(UUID conductorId) throws SQLException;
+
+    List<Trip> findTripsByBus(int busId) throws SQLException;
+
+    List<Trip> findTripsByRoute(int routeId) throws SQLException;
+
+    List<Trip> findTripsByTime(LocalTime from, LocalTime to) throws SQLException;
+
+    //Validation
+    public boolean checkDriver(UUID driverId, LocalTime departure, LocalTime arrival) throws SQLException;
+
+    public boolean checkBus(int busId, LocalTime departure, LocalTime arrival) throws SQLException;
+
+    public boolean checkConductor(UUID conductorId, LocalTime departure, LocalTime arrival) throws SQLException;//kiem tra validation o service
+}
